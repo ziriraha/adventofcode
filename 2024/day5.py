a308f408c8abcdbda96800a34b9f05b6a2ead1f4
Ziri Raha <45337531+ziriraha@users.noreply.github.com>
2024-12-05 15:12:41 +0100

    update: day5 2024

--- 2024/day5.py
+++ 2024/day5.py
@@ -5,7 +5,7 @@ def read_input():
     with open("day5.txt") as f:
         data = f.read()
         order, manuals = data.split("\n\n")
-        return order.splitlines(), manuals.splitlines()
+        return parse_ordering(order.splitlines()), [m.split(",") for m in manuals.splitlines()]
 
 def parse_ordering(rules):
     dRules = defaultdict(list)
@@ -14,27 +14,29 @@ def parse_ordering(rules):
         dRules[b].append(a)
     return dict(dRules)
 
-def check_order(rules, pages): return all(not page in rules.setdefault(pages[i], []) for i in range(len(pages)) for page in pages[:i])
+def check_order(rules, pages): 
+    return all(not page in rules.setdefault(pages[i], []) 
+               for i in range(len(pages)) 
+               for page in pages[:i])
 
-def part1(rules, manuals): return sum([int(manual[len(manual)//2]) for manual in manuals if check_order(rules, manual)])
+def part1(rules, manuals): 
+    return sum([int(manual[len(manual)//2]) 
+                for manual in manuals if check_order(rules, manual)])
 
-def compare_pages(page1, page2, rules):
-    if page1 in rules.setdefault(page2, []):
-        return 1
-    if page2 in rules.setdefault(page1, []):
-        return -1
-    else:
-        return 0
+def compare_pages(page1, page2, rules): 
+    return (1 if page1 in rules.setdefault(page2, []) 
+            else -1 if page2 in rules.setdefault(page1, []) 
+            else 0)
 
-
-def part2(rules, manuals):
-    unordered = [manual for manual in manuals if not check_order(rules, manual)]
-    ordered = [sorted(manual, key=cmp_to_key(lambda x, y: compare_pages(x, y, rules))) for manual in unordered]
-    return sum([int(manual[len(manual)//2]) for manual in ordered])
+def part2(rules, manuals): 
+    return sum([int(sorted(manual, 
+                    key=cmp_to_key(lambda x, y: compare_pages(x, y, rules)))
+            [len(manual)//2]) 
+        for manual in 
+            [manual for manual in 
+                manuals if not check_order(rules, manual)]])
 
 if __name__ == "__main__":
-    order, manuals = read_input()
-    rules = parse_ordering(order)
-    manuals = [m.split(",") for m in manuals]
+    rules, manuals = read_input()
     print(part1(rules, manuals))
     print(part2(rules, manuals))
